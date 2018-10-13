657107469ec00be1cc4c69bc901da2ef28466875
Yova <10067268+yovadev@users.noreply.github.com>
2018-10-13 10:37:34 -0600

    Initial commit

---
+++ src/dosclases/nuevaClases.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dosclases;
+
+/**
+ *
+ * @author Yova
+ */
+public class nuevaClases {
+    public void mensajito(){
+    System.out.println("este es un mensaje");
+    }
+    
+    public void mensajito2(){
+    System.out.println("otro mensaje");
+    }
+    
+}
